eb58a4a34aa853e88dda732264524d1d55100bbb
ynoTL <tonylu23@gamil.com>
2021-03-22 21:16:28 -0400

    create and render a simple HTML table

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,40 +1,36 @@
-import React from 'react';
-// import Table from './Table';
+import React from 'react'
+import Table from './Table'
 // import Form from './Form';
 
 class LinkContainer extends React.Component {
-    constructor(props){
-        super(props)
-        /* TODO - Create state object for storing favLinks */
-    }
+  constructor(props) {
+    super(props)
+    /* TODO - Create state object for storing favLinks */
+  }
 
-    removeCharacter = index => {
-        /*
-            TODO - Create logic for setting the state to filter array and remove favLink at index
-        */
-    }
+  removeCharacter = index => {
+    /* TODO - Create logic for setting the state to filter array and remove favLink at index */
+  }
 
-    handleSubmit = favLink => {
-        /*
-            TODO - Create logic to setState and add new favLink to favLinks array in state
-        */
-    }
+  handleSubmit = favLink => {
+    /* TODO - Create logic to setState and add new favLink to favLinks array in state */
+  }
 
-    render() {
+  render() {
+    return (
+      <div className='container'>
+        <h1>My Favorite Links</h1>
+        <p>Add a new url with a name and link to the table.</p>
+        {/*TODO - Add Table Component */}
+        <Table />
 
-        return (
-            <div className="container">
-                <h1>My Favorite Links</h1>
-                <p>Add a new url with a name and link to the table.</p>
-                {/*TODO - Add Table Component */}
-                
-                <br/>
+        <br />
 
-                <h3>Add New</h3>
-                {/*TODO - Add Form Component */}
-            </div>
-        );
-    }
+        <h3>Add New</h3>
+        {/*TODO - Add Form Component */}
+      </div>
+    )
+  }
 }
 
-export default LinkContainer;
+export default LinkContainer
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React from 'react'
+import { render } from 'react-dom'
 
-const TableHeader = () => { 
-    // boilerplate table header functional component
+const TableHeader = () => {
+  // boilerplate table header functional component
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>URL</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  )
+}
+
+const TableBody = props => {
+  // boilerplate table body functional component
+  // we use Array.map to create table rows from LinkData passed via props
+  const rows = props.linkData.map((row, index) => {
     return (
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>URL</th>
-                <th>Remove</th>
-            </tr>
-        </thead>
-    );
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>
+          <a href={row.URL}>{row.URL}</a>
+        </td>
+        <td>
+          <button onClick={() => props.removeLink(index)}>Delete</button>
+        </td>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
 }
 
-const TableBody = props => { 
-    // boilerplate table body functional component 
-    // we use Array.map to create table rows from LinkData passed via props
-    const rows = props.linkData.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.name}</td>
-                <td><a href={row.URL}>{row.URL}</a></td>
-                <td><button onClick={() => props.removeLink(index)}>Delete</button></td>
-            </tr>
-        );
-    });
-
-    return <tbody>{rows}</tbody>;
+const Table = props => {
+  //   const { linkData, removeLink } = props
+  {
+    /*TODO - return <table> component, TableHeader and TableBody  */
+    return (
+      <table>
+        <TableHeader />
+        <TableBody linkData={[]} />
+      </table>
+    )
+  }
 }
 
-const Table = (props) => {
-//    const { linkData, removeLink } = props;
-    {/*TODO - return <table> component, TableHeader and TableBody  */}
-
-}
-
-export default Table;
+export default Table
